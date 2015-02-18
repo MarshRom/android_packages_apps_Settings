89ec25822da0354bab94584cd61649e1d7e81c61
jkl5616 <jkl5616@gmail.com>
2015-02-18 15:04:43 +0000

    Add animation category & Listanimation Views and Interpolator [2/2]
    Change-Id: I66b3e175c96376276af25b4d611d10c34566cf59

--- src/com/android/settings/marshrom/animation/AnimationControls.java
+++ src/com/android/settings/marshrom/animation/AnimationControls.java
@@ -1,22 +1,4 @@
-/*
- * Copyright (C) 2014 AOKP
- *
- * Modified by crDroid Android
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.android.settings.rr.animation;
+package com.android.settings.marshrom.animation;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -29,11 +11,11 @@ import android.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.rr.animation.AnimBarPreference;
+import com.android.settings.marshrom.animation.AnimBarPreference;
 import com.android.settings.R;
 
 import com.android.internal.logging.MetricsLogger;
-import com.android.internal.util.benzo.AwesomeAnimationHelper;
+import com.android.internal.util.du.AwesomeAnimationHelper;
 
 import java.util.Arrays;
 
