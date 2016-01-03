95d4051ffef883c46f5bcd117fe157d26f364ffd
Nathan Benis <nbenis106@gmail.com>
2016-01-03 08:41:46 +0000

    Clean Up Lockscreen Weather (items & layout) [2/2]
    *remove timestamp toggle
    
    Change-Id: I3af29c61bda981b8d691b38d23292132260e7385

--- src/com/android/settings/MarshSettings.java
+++ src/com/android/settings/MarshSettings.java
@@ -72,7 +72,6 @@ public class MarshSettings extends SettingsPreferenceFragment
     private static final String PREF_SHOW_WEATHER = "weather_show_weather";
     private static final String PREF_SHOW_LOCATION = "weather_show_location";
     private static final String PREF_STATUSBAR_WEATHER = "status_bar_show_weather";
-    private static final String PREF_SHOW_TIMESTAMP = "weather_show_timestamp";
     private static final String PREF_CONDITION_ICON = "weather_condition_icon";
     private static final String PREF_COLORIZE_ALL_ICONS = "weather_colorize_all_icons";
     private static final String PREF_TEXT_COLOR = "weather_text_color";
@@ -86,7 +85,6 @@ public class MarshSettings extends SettingsPreferenceFragment
 
     private SwitchPreference mShowWeather;
     private SwitchPreference mShowLocation;
-    private SwitchPreference mShowTimestamp;
     private SwitchPreference mShowStatusbarWeather;
     private ListPreference mConditionIcon;
     private SwitchPreference mColorizeAllIcons;
@@ -172,11 +170,6 @@ public class MarshSettings extends SettingsPreferenceFragment
                     Settings.System.STATUS_BAR_SHOW_WEATHER, 0) == 1);
             mShowStatusbarWeather.setOnPreferenceChangeListener(this);
 
-            mShowTimestamp = (SwitchPreference) findPreference(PREF_SHOW_TIMESTAMP);
-            mShowTimestamp.setChecked(Settings.System.getInt(mResolver,
-                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP, 0) == 1);
-            mShowTimestamp.setOnPreferenceChangeListener(this);
-
             mConditionIcon = (ListPreference) findPreference(PREF_CONDITION_ICON);
             mConditionIcon.setValue(String.valueOf(conditionIcon));
             mConditionIcon.setSummary(mConditionIcon.getEntry());
@@ -196,7 +189,6 @@ public class MarshSettings extends SettingsPreferenceFragment
         } else {
             removePreference(PREF_SHOW_LOCATION);
             removePreference(PREF_STATUSBAR_WEATHER);
-            removePreference(PREF_SHOW_TIMESTAMP);
             removePreference(PREF_STATUSBAR_WEATHER);
             removePreference(PREF_CONDITION_ICON);
             removePreference(PREF_COLORIZE_ALL_ICONS);
@@ -293,12 +285,6 @@ public class MarshSettings extends SettingsPreferenceFragment
                     Settings.System.STATUS_BAR_SHOW_WEATHER,
                     value ? 1 : 0);
             return true;
-        } else if (preference == mShowTimestamp) {
-            value = (Boolean) newValue;
-            Settings.System.putInt(mResolver,
-                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP,
-                    value ? 1 : 0);
-            return true;
         } else if (preference == mConditionIcon) {
             int intValue = Integer.valueOf((String) newValue);
             int index = mConditionIcon.findIndexOfValue((String) newValue);
@@ -389,8 +375,6 @@ public class MarshSettings extends SettingsPreferenceFragment
                                     Settings.System.LOCK_SCREEN_SHOW_WEATHER, 1);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.LOCK_SCREEN_SHOW_WEATHER_LOCATION, 1);
-                            Settings.System.putInt(getOwner().mResolver,
-                                    Settings.System.LOCK_SCREEN_SHOW_WEATHER_TIMESTAMP, 1);
                             Settings.System.putInt(getOwner().mResolver,
                                     Settings.System.LOCK_SCREEN_WEATHER_CONDITION_ICON, 2);
                             Settings.System.putInt(getOwner().mResolver,
