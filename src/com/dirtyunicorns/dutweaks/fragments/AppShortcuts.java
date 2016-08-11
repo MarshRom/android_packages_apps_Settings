c3bbefc4d7f12afd29a6e18268b263da28ba4f0e
Slydelix <slydelix@gmail.com>
2016-08-11 23:21:26 +0200

    Update AppShortcuts.java

--- src/com/dirtyunicorns/dutweaks/fragments/AppShortcuts.java
+++ src/com/dirtyunicorns/dutweaks/fragments/AppShortcuts.java
@@ -43,7 +43,7 @@ public class AppShortcuts extends SettingsPreferenceFragment implements
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.appshortcuts);
+        addPreferencesFromResource(R.xml.marsh_settings);
 
         PreferenceScreen prefSet = getPreferenceScreen();
 
